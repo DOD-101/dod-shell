ad10f7b2ab1c12a7ce36f3e80cf98ee2eecb3da5
David Thievon <pdkfan@gmail.com>
2025-08-16 20:07:24 +0200

    feat(test-launcher): add command-line argument to have initial search
    THIS IS NOT READY TO MERGE ONTO dev OR master.
    
    There is missing support for this in the cli. And needs to be tested.

--- crates/launcher/src/lib.rs
+++ crates/launcher/src/lib.rs
@@ -60,7 +60,7 @@ pub enum AppMsg {
 /// Generated with [macro@relm4::component].
 #[relm4::component(pub)]
 impl SimpleComponent for App {
-    type Init = ();
+    type Init = Option<String>;
     type Input = AppMsg;
     type Output = ();
 
@@ -102,7 +102,7 @@ impl SimpleComponent for App {
     }
 
     fn init(
-        _: Self::Init,
+        search_term: Self::Init,
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
@@ -141,10 +141,8 @@ impl SimpleComponent for App {
 
         action_group.register_for_widget(&widgets.main_window);
 
-        // "Search" once to populate results
-        let _ = sender
-            .input_sender()
-            .send(AppMsg::SearchUpdate("".to_string()));
+        let initial_search = search_term.unwrap_or_default();
+        widgets.main_entry.set_text(&initial_search);
 
         relm4::set_global_css(&common::get_css());
         ComponentParts { model, widgets }
--- crates/launcher/src/main.rs
+++ crates/launcher/src/main.rs
@@ -1,8 +1,16 @@
+use std::env;
+
 use launcher::App;
 use relm4::RelmApp;
 
 fn main() {
+    let search_term = env::args().nth(1);
     simple_logger::SimpleLogger::new().env().init().unwrap();
     let app = RelmApp::new("dod-shell.launcher");
-    app.run::<App>(());
+
+    // Running using `with_args` to stop gtk errors caused by trying to parse the command-line
+    // arguments itself
+    //
+    // See: https://relm4.org/book/stable/cli.html
+    app.with_args(Vec::new()).run::<App>(search_term);
 }
