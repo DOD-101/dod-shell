2f29c0319c3687a471a77a5d6b91d5e185e6a491
David Thievon <pdkfan@gmail.com>
2025-08-29 21:49:06 +0200

    feat(nix): add systemd services through hm

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -18,6 +18,16 @@ let
       ) self.packages.${pkgs.stdenv.hostPlatform.system}
     );
 
+  getComponent =
+    pname:
+    let
+      component = lib.lists.findFirst (p: p.pname == pname) null cfg.components;
+    in
+    if builtins.isNull component then
+      throw "component ${pname} not found in dod-shell.components"
+    else
+      component;
+
 in
 {
 
@@ -33,7 +43,10 @@ in
     scss = lib.mkOption {
       type = lib.types.str;
       default = "";
-      description = "SCSS for style.scss";
+      description = ''
+        SCSS written to 
+        {file}`$XDG_CONFIG_HOME/dod-shell/style.scss`.
+      '';
     };
 
     settings = lib.mkOption {
@@ -42,6 +55,21 @@ in
       description = ''
         Configuration written to 
         {file}`$XDG_CONFIG_HOME/dod-shell/config.toml`.
+      '';
+    };
+
+    systemd-services = lib.mkOption {
+      type = with lib.types; listOf str;
+      default = map (p: p.pname) cfg.components;
+      description = ''
+        Systemd services to create / enable
+
+        By default systemd services are created for all components 
+        that are enabled via `dod-shell.components` if there are 
+        units associated with them.
+
+        This will also create a `dod-shell.target` systemd target
+        that can be used to control all services simultaneously.
       '';
     };
   };
@@ -53,6 +81,37 @@ in
       "dod-shell/config.toml" = lib.mkIf (cfg.settings != { }) {
         source = tomlFormat.generate "config.toml" cfg.settings;
       };
+    };
+
+    systemd.user = {
+      targets.dod-shell = {
+        Unit = {
+          Description = "dod-shell user services";
+          # Documentation  TODO: add documentation link
+          After = [ "hyprland-session.target" ];
+        };
+        Install = {
+          WantedBy = [ "hyprland-session.target" ];
+        };
+      };
+      services =
+        lib.mkIf (cfg.systemd-services != [ ]) { }
+        // lib.mkIf (lib.lists.any (a: a == "dod-shell-bar") cfg.systemd-services) {
+          dod-shell-bar = {
+            Unit = {
+              Description = "dod-shell bar component service";
+            };
+            Service = {
+              ExecStart = "${getComponent "dod-shell-bar"}/bin/dod-shell-bar";
+              Type = "exec";
+              Restart = "on-failure";
+              RestartSec = 3;
+            };
+            Install = {
+              WantedBy = [ "dod-shell.target" ];
+            };
+          };
+        };
     };
   };
 }
