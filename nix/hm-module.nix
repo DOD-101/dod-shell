0cf29608b4240c3177cdf6d853540d021b2c3774
David Thievon <david.thievon@proton.me>
2026-05-05 10:04:42 +0200

    fix(nix): hm systemd units

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -24,7 +24,7 @@ let
     let
       component = lib.lists.findFirst (p: p.pname == pname) null cfg.components;
     in
-    if builtins.isNull component then
+    if isNull component then
       throw "component ${pname} not found in dod-shell.components"
     else
       component;
@@ -62,7 +62,7 @@ in
         type = lib.types.str;
         default = "";
         description = ''
-          SCSS written to 
+          SCSS written to
           {file}`$XDG_CONFIG_HOME/dod-shell/style.scss`.
 
           This is passed to `xdg.configFile."style.scss".text`.
@@ -72,7 +72,7 @@ in
         type = with lib.types; nullOr path;
         default = null;
         description = ''
-          SCSS file linked to 
+          SCSS file linked to
           {file}`$XDG_CONFIG_HOME/dod-shell/style.scss`.
 
           Passed to `xdg.configFile."style.scss".source`.
@@ -85,7 +85,7 @@ in
         type = tomlFormat.type;
         default = { };
         description = ''
-          Configuration written to 
+          Configuration written to
           {file}`$XDG_CONFIG_HOME/dod-shell/config.toml`.
 
           Passed to `xdg.configFile."config.toml".source` after being
@@ -96,7 +96,7 @@ in
         type = with lib.types; nullOr path;
         default = null;
         description = ''
-          TOML file linked to 
+          TOML file linked to
           {file}`$XDG_CONFIG_HOME/dod-shell/config.toml`.
 
           Passed to `xdg.configFile."config.toml".source`.
@@ -109,7 +109,7 @@ in
         type = jsonFormat.type;
         default = { };
         description = ''
-          Configuration written to 
+          Configuration written to
           {file}`$XDG_CONFIG_HOME/dod-shell/layouts.json`.
 
           Passed to `xdg.configFile."layouts.json".source` after being
@@ -120,7 +120,7 @@ in
         type = lib.types.path;
         default = ../test/layouts.json;
         description = ''
-          JSON file linked to 
+          JSON file linked to
           {file}`$XDG_CONFIG_HOME/dod-shell/layouts.json`.
 
           Passed to `xdg.configFile."layouts.json".source`.
@@ -134,8 +134,8 @@ in
       description = ''
         Systemd services to create / enable
 
-        By default systemd services are created for all components 
-        that are enabled via `dod-shell.components` if there are 
+        By default systemd services are created for all components
+        that are enabled via `dod-shell.components` if there are
         units associated with them.
 
         This will also create a `dod-shell.target` systemd target
@@ -182,45 +182,33 @@ in
       services =
         let
           if_in_services = name: lib.mkIf (lib.lists.any (a: a == name) cfg.systemd-services);
+
+          mkComponentService =
+            component:
+            if_in_services "dod-shell-${component}" {
+              Unit = {
+                Description = "dod-shell ${component} component service";
+                Requires = [ "dod-shell-daemon.service" ];
+                After = [ "dod-shell-daemon.service" ];
+                PartOf = [ "dod-shell.target" ];
+              };
+              Service = {
+                ExecStart = "${getComponent "dod-shell-${component}"}/bin/dod-shell-${component}";
+                Type = "exec";
+                Restart = "on-failure";
+                RestartSec = 3;
+              };
+              Install = {
+                WantedBy = [ "dod-shell.target" ];
+              };
+            };
         in
-        {
-          dod-shell-bar = if_in_services "dod-shell-bar" {
-            Unit = {
-              Description = "dod-shell bar component service";
-            };
-            Service = {
-              ExecStart = "${getComponent "dod-shell-bar"}/bin/dod-shell-bar";
-              Type = "exec";
-              Restart = "on-failure";
-              RestartSec = 3;
-              Requires = [ "dod-shell-daemon.service" ];
-              After = [ "dod-shell-daemon.service" ];
-            };
-            Install = {
-              WantedBy = [ "dod-shell.target" ];
-            };
-          };
-
-          dod-shell-osk = if_in_services "dod-shell-osk" {
-            Unit = {
-              Description = "dod-shell osk component service";
-            };
-            Service = {
-              ExecStart = "${getComponent "dod-shell-osk"}/bin/dod-shell-osk";
-              Type = "exec";
-              Restart = "on-failure";
-              RestartSec = 3;
-              Requires = [ "dod-shell-daemon.service" ];
-              After = [ "dod-shell-daemon.service" ];
-            };
-            Install = {
-              WantedBy = [ "dod-shell.target" ];
-            };
-          };
-
+        (lib.genAttrs' [ "bar" "osk" ] (c: lib.nameValuePair ("dod-shell-" + c) (mkComponentService c)))
+        // {
           dod-shell-daemon = if_in_services "dod-shell-daemon" {
             Unit = {
               Description = "dod-shell daemon service";
+              PartOf = [ "dod-shell.target" ];
             };
             Service = {
               ExecStart = "${getComponent "dod-shell-daemon"}/bin/dod-shell-daemon";
